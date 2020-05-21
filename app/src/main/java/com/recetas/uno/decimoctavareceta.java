2b27d750ea55fcbfe10a0c4bb0ad075ced260a6f
pedrosc1967 <pedrosc1967@gmail.com>
2020-05-21 15:12:16 +0200

    Añadidas imágenes a las recetas

--- app/src/main/java/com/recetas/uno/decimoctavareceta.java
+++ app/src/main/java/com/recetas/uno/decimoctavareceta.java
@@ -10,14 +10,40 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+import com.facebook.ads.AdSize;
+import com.facebook.ads.AdView;
+import com.facebook.ads.AudienceNetworkAds;
+import com.facebook.ads.*;
 
 public class decimoctavareceta extends Activity {
+
+    private AdView adView;
 
     /** Called when the activity is first created. */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.decimoctavareceta);
-            };
+
+        // Initialize the Audience Network SDK
+        AudienceNetworkAds.initialize(this);
+
+        // Instantiate an AdView object.
+        // NOTE: The placement ID from the Facebook Monetization Manager identifies your App.
+        // To get test ads, add IMG_16_9_APP_INSTALL# to your placement id. Remove this when your app is ready to serve real ads.
+
+        adView = new AdView(this, "1159067144426774_1159067394426749", AdSize.BANNER_HEIGHT_50);
+
+        // Find the Ad Container
+        LinearLayout adContainer = (LinearLayout) findViewById(R.id.banner_container);
+
+        // Add the ad view to your activity layout
+        adContainer.addView(adView);
+
+        // Request an ad
+        adView.loadAd();
+    };
             
 @Override
 public boolean onCreateOptionsMenu(Menu menu){
@@ -51,6 +77,12 @@ public boolean onOptionsItemSelected(MenuItem item) {
         default:
         return super.onOptionsItemSelected(item);
             }
-   }            
-
+   }
+    @Override
+    protected void onDestroy() {
+        if (adView != null) {
+            adView.destroy();
+        }
+        super.onDestroy();
+    }
 }
